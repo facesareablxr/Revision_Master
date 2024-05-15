4925aba84d7c07af0f3697656b88938a8651f41b
lad48 <lad48@aber.ac.uk>
2024-05-15 22:08:07 +0100

    Forgot to reorg gradle and comment it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,37 +55,41 @@ android {
 }
 
 dependencies {
-    // Standard dependencies from project start
-    // This dependency enables data binding in Compose UI, simplifying data flow between UI and underlying data sources.
+    // Data binding for Compose UI, simplifying data flow.
     implementation("androidx.databinding:adapters:3.2.0-alpha11")
-    // Provides core functionalities for building AppCompat applications, supporting older Android versions
+
+    // Core functionalities for building AppCompat applications.
     implementation("androidx.appcompat:appcompat:1.6.1")
-    // This dependency offers the latest Material Design 3 components for building modern and consistent user interfaces.
+
+    // Material Design 3 components for modern UIs.
     implementation("androidx.compose.material3:material3:1.2.1")
-    // Required for running unit tests in your project.
+
+    // For running unit tests.
     implementation("androidx.test:runner:1.5.2")
-    //  Provides additional Material Design components and themes for UI, old version, but still useful.
+
+    // Additional Material Design components and themes.
     implementation("com.google.android.material:material:1.12.0")
 
-
-    // Provides libraries for implementing efficient pagination in app, handling large datasets.
+    // Libraries for efficient pagination.
     implementation("androidx.paging:paging-common-android:3.3.0-rc01")
 
-    // Integrates Firebase Authentication features like user login and registration.
+    // Firebase Authentication integration.
     implementation("androidx.paging:paging-common-android:3.3.0-rc01")
 
+    // Animation graphics for Compose.
     implementation("androidx.compose.animation:animation-graphics-android:1.6.7")
+
+    // Core of CameraX library.
     implementation("androidx.camera:camera-core:1.3.3")
+
+    // Coil integration for Compose.
     implementation("io.coil-kt:coil-compose:2.6.0")
 
-    //Firebase Implementation
-    // Enables displaying in-app messaging from Firebase to users.
+    // Firebase dependencies
     implementation("com.google.android.gms:play-services-cast-framework:21.4.0")
     implementation("com.google.firebase:firebase-inappmessaging-display:21.0.0")
-    // Allows access to Firebase Realtime Database, a NoSQL database solution.
     implementation("com.google.firebase:firebase-database:21.0.0")
     implementation("com.google.firebase:firebase-auth:23.0.0")
-    // Provides access to Firestore, a document-oriented NoSQL database solution for Firebase in Kotlin.
     implementation("com.google.firebase:firebase-firestore-ktx:25.0.0")
     implementation("com.google.firebase:firebase-ml-vision:24.1.0")
     implementation("com.google.firebase:firebase-storage-ktx:21.0.0")
@@ -93,85 +97,85 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("com.google.firebase:firebase-storage:21.0.0")
 
-
-    //Compose Platform and Core Dependencies
-    // This line defines the Bill of Materials (BOM) for Compose, ensuring all Compose libraries used are compatible versions.
-    platform("androidx.compose:compose-bom:2024.05.00")
-    // Provides essential Kotlin extensions for core Android functionalities.
+    // Compose Platform and Core Dependencies
+    // Bill of Materials (BOM) for Compose.
     platform("androidx.compose:compose-bom:2024.05.00")
 
+    // Kotlin extensions for core Android.
     implementation("androidx.core:core-ktx:1.13.1")
-    // Offers lifecycle-aware components for managing data across different UI lifecycles in Kotlin.
+
+    // Lifecycle components for managing data across UI lifecycles.
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    // Enables building Compose activities, the foundation for the app screens.
+
+    // Compose activities for app screens.
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.activity:activity-ktx:1.9.0")
-    // The core library for building user interfaces with Compose.
+
+    // Core library for building UI with Compose.
     implementation ("androidx.compose.ui:ui:1.6.7")
-    // Provides foundational building blocks for the Compose UI like layouts, drawables, and interactions.
+
+    // Foundational building blocks for Compose UI.
     implementation ("androidx.compose.foundation:foundation:1.6.7")
-    //  Provides additional Material Design components and themes for UI, old version, but still useful, not completely necessary as I have the others but still here.
+
+    // Material Design components for Compose UI.
     implementation ("androidx.compose.material:material:1.6.7")
     implementation("androidx.compose.material:material-icons-extended")
-    // Enables implementing navigation within the Compose app.
+
+    // Navigation in Compose app.
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    //Testing Dependencies:
-    // The classic JUnit library for writing unit tests.
+    // Testing Dependencies
+    // JUnit for unit tests.
     testImplementation("junit:junit:4.13.2")
-    // Enables writing UI tests for your Compose UI.
+
+    // Mockito for UI tests.
     testImplementation("org.mockito:mockito-core:3.3.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    // Provides testing utilities for navigation within Compose apps.
+
+    // Utilities for navigation testing.
     implementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.7")
-    // A library for asserting test conditions in a concise and readable way.
+
+    // Assertion library.
     androidTestImplementation("com.google.truth:truth:1.1.3")
-    // Required for UI tooling features in debug builds.
+
+    // Debugging tools.
     debugImplementation("androidx.compose.ui:ui-tooling")
-    // Necessary for UI test manifest generation in debug builds.
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    // Provides integration between Compose and LiveData, a lifecycle-aware way to observe data changes.
+    // Integration between Compose and LiveData.
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
+    // ViewModels with Compose.
     implementation("androidx.compose.runtime:runtime-livedata")
-    // Enables using ViewModels with Compose, managing UI state across lifecycle changes.
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
 
-    // Foundation for working with coroutines, enabling asynchronous programming.
+    // Coroutines for asynchronous programming.
     implementation("io.github.vanpra.compose-material-dialogs:datetime:0.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    // Integrates coroutines with Android's UI framework.
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
+    // LiveData for UI state management.
+    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:2.7.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
 
-    // Enables using ViewModels for UI state management in Compose.
-    implementation("io.coil-kt:coil-compose:2.6.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    // Provides LiveData, a lifecycle-aware data holder.
-    implementation ("androidx.lifecycle:lifecycle-livedata-core-ktx:2.7.0")
-    // Bridges LiveData with Compose for seamless observation.
-    implementation ("androidx.compose.runtime:runtime-livedata:1.6.7")
-
-    //Firebase Implementation
+    // Firebase BOM.
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
 
-    //Image implementation
+    // Image loading libraries.
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.github.bumptech.glide:compose:1.0.0-alpha.5")
     implementation("com.squareup.picasso:picasso:2.5.2")
     implementation("io.coil-kt:coil-gif:2.1.0")
 
+    // More Compose lifecycle components.
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
+    // ML Kit dependencies.
     implementation("com.google.mlkit:text-recognition:16.0.0")
-
-    implementation ("com.google.android.gms:play-services-vision:20.1.3")
-    implementation ("com.google.firebase:firebase-ml-vision:24.1.0")
-    implementation ("com.google.firebase:firebase-ml-vision-barcode-model:16.1.2")
-
+    implementation("com.google.android.gms:play-services-vision:20.1.3")
+    implementation("com.google.firebase:firebase-ml-vision-barcode-model:16.1.2")
 }
